1c1a8b37153a5b3d3be2e89fbc1fe19fc278d8cd
Guillermo <guillermortiz21@gmail.com>
2020-12-23 18:57:18 -0600

    Hello world with react

--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -1,16 +1,29 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+// Con js puro podemos crear elementos html con
+// document.createElement()
 
+const element = document.createElement('h1');
+element.innerText   = "Hello, curso react!";
+
+// Ese elemento se pone dentro de un container
+// app es un elemento dentro de public/index.html
 const container = document.getElementById('app');
 
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+// Agregamos el elemento al container
+container.appendChild(element);
+
+
+// Podemos hacer esto mismo, pero con react
+
+// React es análogo a createElement
+// ReactDOM es análogo a appendChild
+
+// Esto es jsx
+const elementReact = <h1>Hello, curso react! Con react!!!</h1>
+
+const containerReact = document.getElementById('app');
+
+// render recibe qué renderizamos y dónde.
+ReactDOM.render(elementReact, containerReact);
