de4c80e2c150a5ea0b985a88e44b564710d9fb1e
Guillermo Ortiz <guillermortiz21@gmail.com>
2020-12-27 14:34:25 -0600

    Hero, Badge and styles added to BadgeNew page

--- platzi-badges/src/pages/BadgeNew.js
+++ platzi-badges/src/pages/BadgeNew.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
+import Badge from '../components/Badge';
+import header from '../images/badge-header.svg';
+
+import './styles/BadgeNew.css';
 
 /*
     Este componente es una página, cuenta con:
@@ -16,9 +20,27 @@ class BadgeNew extends React.Component{
                 {/* Nav bar */}
                 <Navbar/>
                 {/* Hero */}
-                <div></div>
+                <div className="BadgeNew__hero">
+                    <img 
+                        className="img-fluid" 
+                        src={header}
+                        alt="Logo"
+                    />
+                </div>
                 {/* Badge */}
-                <div></div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <Badge
+                                firstName="Guillermo"
+                                lastName="Ortiz"
+                                twitter="guillermo"
+                                jobTitle="Fullstack developer"
+                                avatarUrl="https://www.gravatar.com/avatar?d=identicon"
+                            />
+                        </div>
+                    </div>
+                </div>
                 {/* Formulario */}
                 <div></div>
             </div>
