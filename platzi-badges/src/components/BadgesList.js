7e5518f6f316af5f89229c078b2fbe6fa5af9434
Guillermo Ortiz <guillermortiz21@gmail.com>
2020-12-27 19:54:07 -0600

    Adding styles to list of Badges

--- platzi-badges/src/components/BadgesList.js
+++ platzi-badges/src/components/BadgesList.js
@@ -1,16 +1,36 @@
 import React from 'react'
+import './styles/BadgesList.css'
+
+class BadgeListItem extends React.Component{
+  render(){
+    return(
+      <div className="BadgesListItem">
+        <img
+          className="BadgesListItem__avatar"
+          src={this.props.badge.avatarUrl} 
+          alt="Avatar"
+        />
+        <div>
+          <strong>
+            {this.props.badge.firstName} {this.props.badge.lastName}
+          </strong> 
+          <br/>@{this.props.badge.twitter}
+          <br/>{this.props.badge.jobTitle}
+        </div>
+      </div>
+    )
+  }
+}
 
 class BadgesList extends React.Component{
   render(){
     console.log(this.state)
     return(
-      <ul className="list-unstyled">
+      <ul className="BadgesList">
         {this.props.badges.map((badge) =>{
           return(
             <li key={badge.id}>
-              <p>
-                {badge.firstName} {badge.lastName}
-              </p>
+              <BadgeListItem badge={badge}/>
             </li>
           )
         })}
