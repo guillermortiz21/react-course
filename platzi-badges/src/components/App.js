f16ce7a7070fd3b67581a53abd018adbf1645134
Guillermo Ortiz <guillermortiz21@gmail.com>
2021-01-01 20:37:16 -0600

    Adding a 404 not found page

--- platzi-badges/src/components/App.js
+++ platzi-badges/src/components/App.js
@@ -4,6 +4,7 @@ import Layout from '../components/Layout';
 
 import BadgeNew from '../pages/BadgeNew';
 import Badges from '../pages/Badges';
+import NotFound from '../pages/NotFound';
 
 
 function App(){
@@ -13,6 +14,7 @@ function App(){
                 <Switch>
                     <Route exact path="/badges" component={Badges}/>
                     <Route exact path="/badges/new" component={BadgeNew}/>
+                    <Route component={NotFound}/>
                 </Switch>
             </Layout>
         </BrowserRouter>
