d16ecc0001facfd6455075cb6e54d6174357bc8b
Guillermo Ortiz <guillermortiz21@gmail.com>
2021-01-03 14:02:57 -0600

    Changing badges list from class to stateless functional component

--- platzi-badges/src/components/BadgesList.js
+++ platzi-badges/src/components/BadgesList.js
@@ -3,72 +3,68 @@ import Gravatar from './Gravatar';
 import { Link } from 'react-router-dom'
 import './styles/BadgesList.css'
 
-class BadgeListItem extends React.Component{
-  render(){
-    return(
-      <div className="BadgesListItem">
-        {this.getImageElement()}
-        <div>
-          <strong>
-            {this.props.badge.firstName} {this.props.badge.lastName}
-          </strong> 
-          <br/>@{this.props.badge.twitter}
-          <br/>{this.props.badge.jobTitle}
-        </div>
+function BadgeListItem(props){
+  return(
+    <div className="BadgesListItem">
+      {getImageElement(props)}
+      <div>
+        <strong>
+          {props.badge.firstName} {props.badge.lastName}
+        </strong> 
+        <br/>@{props.badge.twitter}
+        <br/>{props.badge.jobTitle}
       </div>
-    )
-
-  }
-
-  getImageElement = () => {
-    if(this.props.badge.avatarUrl){
-      return(
-        <img
-          className="BadgesListItem__avatar"
-          src={this.props.badge.avatarUrl} 
-          alt="Conf logo"
-        />
-      );
-    }
-    return(
-        <Gravatar
-          className="BadgesListItem__avatar"
-          email={this.props.badge.email}
-          alt="Conf logo"
-        />
-    );
-  }
+    </div>
+  )
 }
 
-class BadgesList extends React.Component{
-  render(){
-    if(this.props.badges.length === 0){
-      return(
-        <div>
-          <h3>No badges were found</h3>
-          <Link className="btn btn-primary" to="/badges/new">
-            Create new badge
-          </Link>
-        </div>
-      );
-    }
+
+function getImageElement(props){
+  if(props.badge.avatarUrl){
     return(
-      <ul className="BadgesList">
-        {this.props.badges.map((badge) =>{
-          return(
-            <li key={badge.id}>
-              <Link 
-                className="text-reset text-decoration-none" 
-                to={`/badges/${badge.id}`}
-              >
-                <BadgeListItem badge={badge}/>
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
-    )
+      <img
+        className="BadgesListItem__avatar"
+        src={props.badge.avatarUrl} 
+        alt="Conf logo"
+      />
+    );
   }
+  return(
+      <Gravatar
+        className="BadgesListItem__avatar"
+        email={props.badge.email}
+        alt="Conf logo"
+      />
+  );
+}
+
+function BadgesList(props){
+  if(props.badges.length === 0){
+    return(
+      <div>
+        <h3>No badges were found</h3>
+        <Link className="btn btn-primary" to="/badges/new">
+          Create new badge
+        </Link>
+      </div>
+    );
+  }
+  return(
+    <ul className="BadgesList">
+      {props.badges.map((badge) =>{
+        return(
+          <li key={badge.id}>
+            <Link 
+              className="text-reset text-decoration-none" 
+              to={`/badges/${badge.id}`}
+            >
+              <BadgeListItem badge={badge}/>
+            </Link>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
 
 export default BadgesList;
